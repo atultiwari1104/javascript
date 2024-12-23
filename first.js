0c60ea024b01ebfefbabde79a06613d8f50b0058
atultiwari1104 <atultiwari23519@gmail.com>
2024-12-23 18:53:59 +0530

    hello

---
+++ first.js
@@ -0,0 +1,24 @@
+/*let num1=parseInt(prompt());
+let num2=parseInt(prompt());
+console.log(num1+num2);
+let num3=parseInt(prompt());
+console.log(num3)
+
+
+area of CSSNumericValue
+squre of num1
+swap teo value */
+   
+
+let r=parseInt(prompt());
+console.log(3.14*r*r);
+let number=parseInt(prompt());
+console.log(number*number);
+
+let a=2
+let b=3
+let c=a
+a=b
+b=c
+console.log(a)
+console.log(b)
---
+++ second.js
@@ -0,0 +1,52 @@
+//even or odd
+/*let num=parseInt(prompt("enter a number:"))
+if(num%2==0){
+    alert("number is even")
+}
+else{
+    alert('number is odd')
+}
+let num2=parseInt(prompt("enter a number:"))
+if(num2>0){
+    alert("number is positive")
+}
+else{
+    alert("number is negative")
+}
+let age=parseInt(prompt("enter a number:"))
+if(age<18){
+    alert("you are not eligible for voating")
+}
+else{
+    alert("you are eligible for voating")
+}
+
+   let num3=parseInt(prompt("enter a number:"))
+   if(num3>0){
+     alert("positive")
+   }
+   else if(num3<0){
+     alert("negative")
+   }
+   else{
+    alert("nuteral")
+   }      */
+   
+
+    let a=parseInt(prompt("enter a number:"))
+    if(a==1){
+       alert (parseInt(prompt("first number"))+ parseInt(prompt("second number")))
+    }
+    else if(a==2){
+        alert (parseInt(prompt("first number"))*parseInt(prompt("second number")))
+    }
+    else if(a==3){
+        alert (parseInt(prompt("first number"))- parseInt(prompt("second number")))
+    }
+    else if(a==3){
+        alert (parseInt(prompt("first number"))/parseInt(prompt("second number")))
+    }
+    else{
+        alert("invalid")
+    }
+
