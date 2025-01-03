f38207c0e6ce6dda6c94715261ebedfbbb476db2
atultiwari1104 <atultiwari23519@gmail.com>
2025-01-03 18:51:28 +0530

    hello

--- dem.js
+++ dem.js
@@ -3,6 +3,7 @@
 //name[index value].innerHtml=using for changing the content with the tages
 //name[index value].textcontent=only for content
 //ECMA==is a standerd for scripting langauge including javascript actionscript 
+//ECMA full form is European Computer Manufacturers Association
 //HOSITING==is a built in a langauge behaviour that moves variable funcation and class decleration to the top of their scope before code executation
 //ES6
 
@@ -14,6 +15,6 @@ Document.write("hello from js")
 
 let content=document.getElementsByClassName("content")
 console.log(content)
-content[0].innerHTML="hello"
+content[0].innerHTML="<i>hello</i>"
 content[1].textContent="hello i am atul"
 
---
+++ dom1.js
@@ -0,0 +1,10 @@
+let h1=document.getElementById('headcontent')
+h1.textContent="hello"
+h1.innerHTML="<i>hello i am developer</i>"
+h1.style.color="red"
+h1.style.backgroundColor="yellow"
+h1.style.padding="20px"
+
+let myimg=document.getElementById("atul")
+myimg.src="hotel.jpeg"
+
