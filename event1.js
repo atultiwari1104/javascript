27f98f7e34d09d16e89423bd7c442c65a6f48c02
atultiwari1104 <atultiwari23519@gmail.com>
2025-01-06 19:10:21 +0530

    hello

---
+++ event1.js
@@ -0,0 +1,17 @@
+function fun2(){
+ let img=document.getElementById("image")
+ img.src="hotel.jpeg"
+}
+function fun1(){
+    let imag=document.getElementById("image")
+    imag.src="atul.jpg"
+}
+
+function red(color){
+    document.body.style.background=color
+}
+
+function drk(){
+    let para=document.getElementById("para")
+    para.classList.add('drk')
+}
