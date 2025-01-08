33d16c6f72be8e820c11a0bd34be4e4dda775438
atultiwari1104 <atultiwari23519@gmail.com>
2025-01-08 19:32:37 +0530

    hello

---
+++ form.js
@@ -0,0 +1,10 @@
+function formData(){
+    let num1=parseInt(document.getElementById("num1").value)
+    let num2=parseInt(document.getElementById("num2").value)
+    let head=document.getElementById("head1")
+    head.innerHTML=num1+num2
+    return false
+}
+
+
+   
