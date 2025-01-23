9112e574134db8b4dc917a290845a318e6e27394
atultiwari1104 <atultiwari23519@gmail.com>
2025-01-23 19:01:35 +0530

    hello

---
+++ array.js
@@ -0,0 +1,25 @@
+/*let ar=[1,"atul",true]
+console.log(ar[2]);
+ for(let a=0;a<5;a++){
+    console.log(ar[a])
+ }
+ for(let vl of ar){
+    console.log(vl)
+ } 
+ let arr=[]
+ for(let a=0;a<5;a++){
+    arr[a]=prompt("enter a number..... stored in array")
+ }
+ console.log(arr)
+
+
+ //write a program to print odd numbers 25 to 1 in an array
+ 
+ let array=[]
+ for(let a=25;a>=1;a--){
+  // console.log(array[a])
+   if(a%2==0){
+    console.log(array[a])
+   }
+ }
+*/
